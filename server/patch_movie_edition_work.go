fa6ce43d0118090435cb3466917b22ec94cf20a2
Andy Kreling <krelinga@gmail.com>
2026-01-06 02:48:53 +0000

    implement put and patch on MovieEdition.

--- server/patch_movie_edition_work.go
+++ server/patch_movie_edition_work.go
@@ -2,11 +2,111 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/krelinga/video-catalog/internal"
 	"github.com/krelinga/video-catalog/vcrest"
 )
 
-func (s *Server) PatchMovieEdition(ctx context.Context, request vcrest.PatchMovieEditionRequestObject) (vcrest.PatchMovieEditionResponseObject, error) {
-	return nil, fmt.Errorf("not implemented")
+// PatchMovieEdition updates fields of a movie edition work with the given UUID
+func (s *Server) PatchMovieEdition(ctx context.Context, request vcrest.PatchMovieEditionRequestObject) (outResp vcrest.PatchMovieEditionResponseObject, _ error) {
+	// Validate request.
+	requestUuid, err := uuid.Parse(request.Uuid.String())
+	if err != nil {
+		outResp = vcrest.PatchMovieEdition400JSONResponse{
+			Message: "invalid UUID format",
+		}
+		return
+	}
+	if request.Body == nil {
+		outResp = vcrest.PatchMovieEdition400JSONResponse{
+			Message: "request body is required",
+		}
+		return
+	}
+	if request.Body.EditionType.IsSpecified() && (request.Body.EditionType.IsNull() || request.Body.EditionType.MustGet() == "") {
+		outResp = vcrest.PatchMovieEdition400JSONResponse{
+			Message: "cannot update editionType to be null or empty",
+		}
+		return
+	}
+
+	txn, err := s.Pool.Begin(ctx)
+	if err != nil {
+		outResp = vcrest.PatchMovieEdition500JSONResponse{
+			Message: fmt.Sprintf("failed to begin transaction: %v", err),
+		}
+		return
+	}
+	defer txn.Rollback(ctx)
+
+	var kind internal.WorkKind
+	var rawBody json.RawMessage
+	row := txn.QueryRow(ctx, `
+		SELECT kind, body
+		FROM works
+		WHERE uuid = $1
+	`, requestUuid)
+	err = row.Scan(&kind, &rawBody)
+	if errors.Is(err, pgx.ErrNoRows) {
+		outResp = vcrest.PatchMovieEdition404JSONResponse{
+			Message: "work not found",
+		}
+		return
+	} else if err != nil {
+		outResp = vcrest.PatchMovieEdition500JSONResponse{
+			Message: fmt.Sprintf("failed to query work: %v", err),
+		}
+		return
+	} else if kind != internal.WorkKindMovieEdition {
+		outResp = vcrest.PatchMovieEdition409JSONResponse{
+			Message: "work is not a movie edition",
+		}
+		return
+	}
+	var body internal.MovieEditionWork
+	if err := json.Unmarshal(rawBody, &body); err != nil {
+		outResp = vcrest.PatchMovieEdition500JSONResponse{
+			Message: fmt.Sprintf("failed to unmarshal work body: %v", err),
+		}
+		return
+	}
+
+	if request.Body.EditionType.IsSpecified() {
+		body.EditionType = request.Body.EditionType.MustGet()
+	}
+
+	rawBody, err = json.Marshal(body)
+	if err != nil {
+		outResp = vcrest.PatchMovieEdition500JSONResponse{
+			Message: fmt.Sprintf("failed to marshal database body: %v", err),
+		}
+		return
+	}
+
+	_, err = txn.Exec(ctx, `
+		UPDATE works
+		SET body = $2
+		WHERE uuid = $1
+	`, requestUuid, rawBody)
+	if err != nil {
+		outResp = vcrest.PatchMovieEdition500JSONResponse{
+			Message: fmt.Sprintf("failed to update work: %v", err),
+		}
+		return
+	}
+
+	if err := txn.Commit(ctx); err != nil {
+		outResp = vcrest.PatchMovieEdition500JSONResponse{
+			Message: fmt.Sprintf("failed to commit transaction: %v", err),
+		}
+		return
+	}
+
+	outResp = vcrest.PatchMovieEdition200Response{}
+	return
 }
--- server/put_movie_edition_work.go
+++ server/put_movie_edition_work.go
@@ -2,12 +2,75 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/krelinga/video-catalog/internal"
 	"github.com/krelinga/video-catalog/vcrest"
 )
 
-// PutMovieEdition creates a new movie edition work with the given UUID
-func (s *Server) PutMovieEdition(ctx context.Context, request vcrest.PutMovieEditionRequestObject) (vcrest.PutMovieEditionResponseObject, error) {
-	return nil, fmt.Errorf("not implemented")
+// PutMovieEdition creates or updates a movie edition work with the given UUID
+func (s *Server) PutMovieEdition(ctx context.Context, request vcrest.PutMovieEditionRequestObject) (outResp vcrest.PutMovieEditionResponseObject, _ error) {
+	// Validate request.
+	requestUuid, err := uuid.Parse(request.Uuid.String())
+	if err != nil {
+		outResp = vcrest.PutMovieEdition400JSONResponse{
+			Message: "invalid UUID format",
+		}
+		return
+	}
+	if request.Body == nil {
+		outResp = vcrest.PutMovieEdition400JSONResponse{
+			Message: "request body is required",
+		}
+		return
+	}
+	var body internal.MovieEditionWork
+	if !request.Body.EditionType.IsSpecified() || request.Body.EditionType.IsNull() || request.Body.EditionType.MustGet() == "" {
+		outResp = vcrest.PutMovieEdition400JSONResponse{
+			Message: "non-empty editionType is required",
+		}
+		return
+	} else {
+		body.EditionType = request.Body.EditionType.MustGet()
+	}
+
+	bodyRaw, err := json.Marshal(body)
+	if err != nil {
+		outResp = vcrest.PutMovieEdition500JSONResponse{
+			Message: fmt.Sprintf("failed to marshal database body: %v", err),
+		}
+		return
+	}
+
+	row := s.Pool.QueryRow(ctx, `
+		INSERT INTO works (uuid, kind, body)
+		VALUES ($1, $2, $3)
+		ON CONFLICT (uuid) DO UPDATE
+		SET body = EXCLUDED.body
+		WHERE works.kind = $2
+		RETURNING xmax`,
+		requestUuid, internal.WorkKindMovieEdition, bodyRaw)
+	var xmax uint32
+	if err := row.Scan(&xmax); errors.Is(err, pgx.ErrNoRows) {
+		outResp = vcrest.PutMovieEdition409JSONResponse{
+			Message: "work with given UUID already exists with different kind",
+		}
+		return
+	} else if err != nil {
+		outResp = vcrest.PutMovieEdition500JSONResponse{
+			Message: fmt.Sprintf("failed to insert/update work: %v", err),
+		}
+		return
+	}
+
+	if xmax == 0 {
+		outResp = vcrest.PutMovieEdition201Response{}
+	} else {
+		outResp = vcrest.PutMovieEdition200Response{}
+	}
+	return
 }
